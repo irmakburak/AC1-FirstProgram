5a21d67574021471f94929c3a6b209cb8654a87a
burak <burak0irm@gmail.com>
2025-10-08 17:42:26 +0200

    Add project files.

---
+++ AC1-FirstProgram/FirstProgram.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	cout << "Enter an integer number:";
+	cin >> a;
+	cout << "Enter an integer number:";
+	cin >> b;
+	cout << "The sum is equal" << a + b << endl;
+	}
+	
