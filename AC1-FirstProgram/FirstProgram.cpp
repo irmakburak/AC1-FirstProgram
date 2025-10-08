815e9286c28ffd9f4e583fb49a3fa8cc6b949a51
burak <burak0irm@gmail.com>
2025-10-08 18:00:44 +0200

    new

--- AC1-FirstProgram/FirstProgram.cpp
+++ AC1-FirstProgram/FirstProgram.cpp
@@ -1,11 +1,24 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-	int a, b;
-	cout << "Enter an integer number:";
-	cin >> a;
-	cout << "Enter an integer number:";
-	cin >> b;
-	cout << "The sum is equal" << a + b << endl;
-	}
-	
+    int a, b;
+    cout << "Enter first integer: ";
+    cin >> a;
+    cout << "Enter second integer: ";
+    cin >> b;
+
+    cout << "Sum: " << a + b << endl;
+    cout << "Difference: " << a - b << endl;
+    cout << "Product: " << a * b << endl;
+
+    cout << "Divisors of " << a << ": ";
+    for (int i = 1; i <= abs(a); i++) {
+        if (a % i == 0) {
+            cout << i << " " << -i << " ";
+        }
+    }
+    cout << endl;
+
+    return 0;
+}
