f49c5b47ce037c0549ca98d1f9d52ccdc42e4be8
burak <burak0irm@gmail.com>
2025-11-12 18:09:48 +0100

    233232

--- ac7-22.11/FileName.cpp
+++ ac7-22.11/FileName.cpp
@@ -1,1 +1,19 @@
-s
+#include <iostream>
+#include "searchUtils.h"
+
+int main() {
+    auto arr = generateSortedVector(10000);
+
+    int linComp, binComp;
+    int key = 10010;
+
+    int linIndex = linearSearch(arr, key, linComp);
+    int binIndex = binarySearch(arr, key, binComp);
+
+    std::cout << "Linear search: index = " << linIndex
+        << ", comparisons = " << linComp << "\n";
+    std::cout << "Binary search: index = " << binIndex
+        << ", comparisons = " << binComp << "\n";
+
+    return 0;
+}
